6903fe1ce395b1c61a43b2bbccc2c6dd71dc6b19
MrHuu <huuphong91@gmail.com>
2018-11-02 23:45:15 +0700

    init commit

--- app/src/main/java/com/example/huu/orderfood/Entities/NhanVienEntity.kt
+++ app/src/main/java/com/example/huu/orderfood/Entities/NhanVienEntity.kt
@@ -1,6 +1,7 @@
 package com.example.huu.orderfood.Entities
 //Đối tượng singleton này dùng để lưu giá trị tạm thời của các trường trong table NHANVIEN
-//mà người dùng đã nhập vào. Để kiểm tra các hoạt động, thao tác CRUD vào SQLiteDatabase
+//mà người dùng đã tương tác. Có chức năng hỗ trợ các phương thức , thao tác CRUD vào SQLiteDatabase
+//liên quan đến NHANVIEN
 object NhanVienEntity {
     var MANV:Int = 0
     var TENDN = ""
